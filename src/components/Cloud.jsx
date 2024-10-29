02018a8bc98da3e72dbbe1b52c06865f7e8a80ec
yeji <skql4128@naver.com>
2024-10-29 15:31:01 +0900

    [feat] cloud 모델 import

---
+++ src/components/Cloud.jsx
@@ -0,0 +1,18 @@
+import { useGLTF } from '@react-three/drei';
+
+export function Cloud({ opacity, ...props }) {
+  const { nodes, materials } = useGLTF('/models/cloud/model.glb');
+  return (
+    <group {...props} dispose={null}>
+      <mesh geometry={nodes.Node.geometry}>
+        <meshStandardMaterial
+          {...materials['lambert2SG.001']}
+          transparent
+          opacity={opacity}
+        />
+      </mesh>
+    </group>
+  );
+}
+
+useGLTF.preload('/models/cloud/model.glb');
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,6 +1,7 @@
 import { OrbitControls, Float } from '@react-three/drei';
 import { Background } from './Background';
 import { Airplane } from './Airplane';
+import { Cloud } from './Cloud';
 
 export const Experience = () => {
   return (
@@ -14,6 +15,24 @@ export const Experience = () => {
           position-y={0.1}
         />
       </Float>
+
+      {/* CLOUDS */}
+      <Cloud opacity={0.5} scale={[0.3, 0.3, 0.3]} position={[-2, 1, -3]} />
+      <Cloud opacity={0.5} scale={[0.2, 0.3, 0.4]} position={[1.5, -0.5, -2]} />
+      <Cloud
+        opacity={0.7}
+        scale={[0.3, 0.3, 0.4]}
+        rotation-y={Math.PI / 9}
+        position={[2, -0.2, -2]}
+      />
+      <Cloud
+        opacity={0.7}
+        scale={[0.4, 0.4, 0.4]}
+        rotation-y={Math.PI / 9}
+        position={[1, -0.2, -12]}
+      />
+      <Cloud opacity={0.7} scale={[0.5, 0.5, 0.5]} position={[-1, 1, -53]} />
+      <Cloud opacity={0.3} scale={[0.8, 0.8, 0.8]} position={[0, 1, -100]} />
     </>
   );
 };
