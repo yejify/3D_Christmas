4fd8e91e0a58bbb32fa5a2b7a37f65abd4016899
yeji <skql4128@naver.com>
2024-10-30 08:40:57 +0900

    [feat] line 생성

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,20 +1,78 @@
-import { OrbitControls, Float } from '@react-three/drei';
-import { Background } from './Background';
+import { Float, PerspectiveCamera, useScroll } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
+import { useMemo, useRef } from 'react';
+import * as THREE from 'three';
 import { Airplane } from './Airplane';
+import { Background } from './Background';
 import { Cloud } from './Cloud';
 
+const LINE_NB_POINTS = 12000;
+
 export const Experience = () => {
+  const curve = useMemo(() => {
+    return new THREE.CatmullRomCurve3(
+      [
+        new THREE.Vector3(0, 0, 0),
+        new THREE.Vector3(0, 0, -10),
+        new THREE.Vector3(-2, 0, -20),
+        new THREE.Vector3(-3, 0, -30),
+        new THREE.Vector3(0, 0, -40),
+        new THREE.Vector3(5, 0, -50),
+        new THREE.Vector3(7, 0, -60),
+        new THREE.Vector3(5, 0, -70),
+        new THREE.Vector3(0, 0, -80),
+        new THREE.Vector3(0, 0, -90),
+        new THREE.Vector3(0, 0, -100),
+      ],
+      false,
+      'catmullrom',
+      0.5
+    );
+  }, []);
+
+  const linePoints = useMemo(() => {
+    return curve.getPoints(LINE_NB_POINTS);
+  }, [curve]);
+
+  const shape = useMemo(() => {
+    const shape = new THREE.Shape();
+    shape.moveTo(0, -0.2);
+    shape.lineTo(0, 0.2);
+
+    return shape;
+  }, [curve]);
+
   return (
     <>
-      <OrbitControls />
-      <Background />
-      <Float floatIntensity={2} speed={2}>
-        <Airplane
-          rotation-y={Math.PI / 2}
-          scale={[0.2, 0.2, 0.2]}
-          position-y={0.1}
-        />
-      </Float>
+      {/* <OrbitControls enableZoom={false} /> */}
+      <group ref={cameraGroup}>
+        <Background />
+
+        <Float floatIntensity={2} speed={2}>
+          <Airplane
+            rotation-y={Math.PI / 2}
+            scale={[0.2, 0.2, 0.2]}
+            position-y={0.1}
+          />
+        </Float>
+      </group>
+
+      {/* LINE */}
+      <group position-y={-2}>
+        <mesh>
+          <extrudeGeometry
+            args={[
+              shape,
+              {
+                steps: LINE_NB_POINTS,
+                bevelEnabled: false,
+                extrudePath: curve,
+              },
+            ]}
+          />
+          <meshStandardMaterial color={'white'} opacity={0.7} transparent />
+        </mesh>
+      </group>
 
       {/* CLOUDS */}
       <Cloud opacity={0.5} scale={[0.3, 0.3, 0.3]} position={[-2, 1, -3]} />
